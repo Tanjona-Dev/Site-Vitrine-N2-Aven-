0e00684165d32b66166c459724002f6193782e5f
Tanjona-Dev <rabemangasoatanjonanasa@gmail.com>
2025-02-13 09:09:12 +0300

    Premier push apres 3 jours de travail

---
+++ index.js
@@ -0,0 +1,61 @@
+let navBar = document.querySelector(".headerS1")
+let boite1 = document.querySelector(".boite1")
+let boite2 = document.querySelector(".boite2")
+let boite3 = document.querySelector(".boite3")
+let boite4 = document.querySelector(".boite4")
+let boite5 = document.querySelector(".boite5")
+let boite6 = document.querySelector(".boite6")
+
+function testeScroll(){
+    document.addEventListener("scroll",()=>{
+       let valeur = window.scrollY
+       if(valeur > 893){
+           navBar.classList.add("toggleNavBar")
+       }else{
+        navBar.classList.remove("toggleNavBar")
+       }
+    })
+
+}
+testeScroll()
+
+function ajouterDesBoites(valeur){
+    valeur.addEventListener("mouseenter", ()=>{
+        valeur.classList.add("boiteDesboites2")
+    })
+    valeur.addEventListener("mouseleave", ()=>{
+       
+        valeur.classList.remove("boiteDesboites2")
+    })
+}
+ajouterDesBoites(boite1)
+ajouterDesBoites(boite2)
+ajouterDesBoites(boite3)
+ajouterDesBoites(boite4)
+ajouterDesBoites(boite5)
+ajouterDesBoites(boite6)
+
+// -----------------------------------------------------------------------------------------------
+
+// PARTIE 5 IMAGE FLEX MOUSEOVER
+
+let home1 = document.querySelector("#home1")
+let home2 = document.querySelector("#home2")
+let home3 = document.querySelector("#home3")
+let home4 = document.querySelector("#home4")
+let home5 = document.querySelector("#home5")
+let hone6 = document.querySelector("#home6")
+let boxDeplacant = document.querySelector("#boxDeplacant")
+
+function homeMouse(){
+    home1.addEventListener("mousemove", (e)=>{
+        console.log(e.clientX);
+        if (e.clientX >= 400)
+       console.log("mety");
+       
+    })
+}
+// homeMouse()
+
+
+
